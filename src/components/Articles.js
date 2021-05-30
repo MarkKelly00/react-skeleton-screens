f5d6a1e269c09faa3dd38c1eaf825029ee11b882
Mark Kelly <big.mako@gmail.com>
2021-05-29 21:20:38 -0700

    created SkeletonElement and styles

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import SkeletonArticle from '../skeletons/SkeletonArticle';
 
 export default function Articles() {
     const [articles, setArticles] = useState(null);
@@ -19,11 +20,11 @@ export default function Articles() {
                 <div className="article" key={article.id}>
                     <h3>{article.title}</h3>
                     <p>{article.body}</p>
-                    
+
                 </div>
             ))}
 
-            {!articles && <div>Loading...</div>}
+            {!articles && [1,2,3,4,5].map((n) => <SkeletonArticle key={n} />)}
         </div>
     )
 }
